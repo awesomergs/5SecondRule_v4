1b2fb3b3405c3314e0e5acdbcf57725e6929761f
awesomergs <george.rohan21@gmail.com>
2026-01-15 16:17:56 -0800

    Initial Commit

---
+++ 5SecondRule_v4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  5SecondRule_v4
+//
+//  Created by Rohan George on 1/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 5SecondRule_v4/_SecondRule_v4App.swift
@@ -0,0 +1,17 @@
+//
+//  _SecondRule_v4App.swift
+//  5SecondRule_v4
+//
+//  Created by Rohan George on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct _SecondRule_v4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
